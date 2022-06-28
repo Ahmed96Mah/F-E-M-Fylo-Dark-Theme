99c8cef7f8351ccb3411bc25436a2dc03eee9b0f
Ahmed Mahmoud Abdalwahab <ahmed96mahmoud1996@gmail.com>
2022-06-28 15:55:42 +0200

    Add TS page logic

---
+++ src/app.ts
@@ -0,0 +1,49 @@
+const processClick = (e: Event) => {
+  if (
+    (e.target as HTMLElement).nodeName === 'BUTTON' &&
+    (e.target as HTMLElement).id === 'endButton'
+  ) {
+    const eMail = document.querySelector('input')!.value;
+    const errParag = document.querySelector('#error')!;
+    if (eMail.match('@')) {
+      const domain = eMail.split('@');
+      // If after the '@', there is a domain name and '.'
+      if (
+        domain[1].split('.')[1] !== undefined &&
+        domain[1].split('.')[0] !== ''
+      ) {
+        // If e-mail domain ends with '.com' && e-mail contains ONLY one '@'
+        if (
+          domain[1].split('.')[1].toLowerCase() === 'com' &&
+          domain[2] === undefined
+        ) {
+          // valid E-Mail
+          if (!errParag.classList.contains('hide')) {
+            errParag.classList.toggle('hide');
+          }
+        } else {
+          //invalid E-Mail
+          if (errParag.classList.contains('hide')) {
+            errParag.classList.toggle('hide');
+          }
+        }
+      } else {
+        //invalid E-Mail
+        if (errParag.classList.contains('hide')) {
+          errParag.classList.toggle('hide');
+        }
+      }
+    } else {
+      //invalid E-Mail
+      if (errParag.classList.contains('hide')) {
+        errParag.classList.toggle('hide');
+      }
+    }
+  }
+};
+
+const init = () => {
+  document.addEventListener('click', processClick);
+};
+
+document.addEventListener('DOMContentLoaded', init);
